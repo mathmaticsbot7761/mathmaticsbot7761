42bd60503088549d6c679e4055213456b5302ae8
mathmaticsbot7761 <mathmaticsbot7761@gmail.com>
2025-03-27 20:33:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ pwimg = "https://graph.org/file/8add8d382169e326f67e0-3bf38f92e52955e977.jpg"
 cpimg = "https://graph.org/file/5ed50675df0faf833efef-e102210eb72c1d5a17.jpg"  
 
 # Define the owner's user ID
-OWNER_ID = 8144269730 # Replace with the actual owner's user ID
+OWNER_ID = 8144269730  # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
 SUDO_USERS = [6854709241,5840594311,7856557198,5034929962]
